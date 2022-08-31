c2260869c6565957e4954c174c5d7671459e6756
Emanuel Ferreira da Silva <emanuelfs.info@gmail.com>
2022-08-30 21:37:58 -0300

    Incluindo testes para CRUD de usuários

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
-module.exports = [1, 2, 3];
+const users = new Map();
+
+var userId = 1;
+
+const insert = userData => {
+    users.set(userId, {
+        id: userId,
+        ...userData,
+    });
+
+    return users.get(userId++);
+};
+
+const select = id => {
+    return users.get(id);
+};
+
+const update = (id, userData) => {
+    let user = users.get(id);
+
+    if (user != undefined) {
+        users.set(user.id, {
+            id: user.id,
+            ...userData
+        });
+
+        return users.get(user.id);
+    }
+
+    return undefined;
+};
+
+const remove = id => {
+    let user = users.get(id);
+
+    if (user != undefined) {
+        users.delete(id);
+
+        return user;
+    }
+
+    return undefined;
+};
+
+module.exports = {
+    insert,
+    select,
+    update,
+    remove,
+};
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,75 @@
 var assert = require('assert');
 
-var arrayList = require('../src/app');
+const {
+    insert,
+    select,
+    update,
+    remove,
+} = require('../src/app');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(arrayList.indexOf(4), -1);
-        });
+const userData1 = {
+    id: 1,
+    name: 'Maria',
+    age: 20,
+    gender: 'Feminino'
+};
+
+const userData2 = {
+    id: 1,
+    name: 'Joana',
+    age: 25,
+    gender: 'Feminino'
+};
+
+const userData3 = {
+    id: 2,
+    name: 'João',
+    age: 25,
+    gender: 'Masculino'
+};
+
+describe('User API', function () {
+    it('POST', function () {
+        assert.equal(JSON.stringify(insert({
+            name: 'Maria',
+            age: 20,
+            gender: 'Feminino',
+        })), JSON.stringify(userData1));
+    });
+
+    it('GET', function () {
+        assert.equal(JSON.stringify(select(1)), JSON.stringify(userData1));
+    });
+
+    it('PUT', function () {
+        assert.equal(JSON.stringify(update(1, {
+            name: 'Joana',
+            age: 25,
+            gender: 'Feminino',
+        })), JSON.stringify(userData2));
+    });
+
+    it('DELETE', function () {
+        assert.equal(JSON.stringify(remove(1)), JSON.stringify(userData2));
+    });
+
+    it('PUT', function () {
+        assert.equal(JSON.stringify(update(1, {
+            name: 'Joana',
+            age: 25,
+            gender: 'Feminino',
+        })), undefined);
+    });
+
+    it('DELETE', function () {
+        assert.equal(JSON.stringify(remove(1)), undefined);
+    });
+
+    it('POST', function () {
+        assert.equal(JSON.stringify(insert({
+            name: 'João',
+            age: 25,
+            gender: 'Masculino',
+        })), JSON.stringify(userData3));
     });
 });
